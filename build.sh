93231859958cecade29218bd8f94e0f3080541cc
Samuel Dalesjö <Dalesjo@users.noreply.github.com>
2022-05-01 22:01:49 +0200

    update build script

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ fi
 # New Version number
 git pull
 
+# dotnet tool install -g dotnet-version-cli
+# https://github.com/skarpdev/dotnet-version-cli
 dotnet version --skip-vcs -f ./TallyClient/TallyClient.csproj patch
 dotnet version --skip-vcs -f ./TallyServer/TallyServer.csproj patch
 
@@ -21,8 +23,8 @@ echo "Commiting changes"
 git add ./TallyClient/TallyClient.csproj
 git add ./TallyServer/TallyServer.csproj
 
-tar -czvf images/tally-client-${VERSION}.tar.gz TallyServer/bin/Release/net6.0/win-x64/
-tar -czvf images/tally-server-${VERSION}.tar.gz TallyClient/bin/Release/net6.0/linux-arm64/publish/
+tar -czvf images/tally-server-${VERSION}.tar.gz TallyServer/bin/Release/net6.0/win-x64/
+tar -czvf images/tally-client-${VERSION}.tar.gz TallyClient/bin/Release/net6.0/linux-arm64/publish/
 
 git commit -m "Built new Version ${VERSION}"
 git push 

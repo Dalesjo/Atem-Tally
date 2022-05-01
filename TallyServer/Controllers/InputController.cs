0c056ad9f94b0db3d4883c0a69699c66a6e216aa
Samuel Dalesjö <Dalesjo@users.noreply.github.com>
2022-05-01 13:19:33 +0200

    Rename TallyServer

---
+++ TallyServer/Controllers/InputController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using TallyServer.Hubs;
+using TallyServer.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TallyServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InputController : ControllerBase
+    {
+        private AtemService AtemService { get; set; }
+        private IHubContext<TallyHub, ITallyClient> TallyHub { get; set; }
+
+        InputController(
+            AtemService atemService,
+            IHubContext<TallyHub, ITallyClient> tallyHub)
+        {
+            AtemService = atemService; ;
+            TallyHub = tallyHub;
+        }
+
+
+        // GET: api/<InputController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<InputController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<InputController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<InputController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<InputController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TallyServer/Hubs/ITallyClient.cs
@@ -0,0 +1,12 @@
+﻿using TallyServer.Contract;
+using TallyShared.Contract;
+
+namespace TallyServer.Hubs
+{
+    public interface ITallyClient
+    {
+        Task ReceiveTally(Tally tally);
+
+        Task RecieveChannel(Input channel);
+    }
+}

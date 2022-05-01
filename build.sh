892a5543550745cedf1f7fee367acf0b8f6dcaa2
Samuel Dalesjö <Dalesjo@users.noreply.github.com>
2022-05-01 21:53:55 +0200

    add build/release script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#if output=$(git status --porcelain) && [ -n "$output" ]; then
+#  echo "You have uncommited changes"
+#  exit 3
+#fi
+
+# New Version number
+git pull
+
+dotnet version --skip-vcs -f ./TallyClient/TallyClient.csproj patch
+dotnet version --skip-vcs -f ./TallyServer/TallyServer.csproj patch
+
+VERSION=`cat ./TallyServer/TallyServer.csproj |  grep -oPm1 "(?<=<Version>)[^<]+" | tr -d '\n'`
+echo "New verison is ${VERSION}"
+
+dotnet publish -c Release TallyClient/TallyClient.csproj
+dotnet publish -c Release TallyServer/TallyServer.csproj
+
+echo "Commiting changes"
+git add ./TallyClient/TallyClient.csproj
+git add ./TallyServer/TallyServer.csproj
+
+tar -czvf images/tally-client-${VERSION}.tar.gz /TallyServer/bin/Release/net6.0/win-x64/
+tar -czvf images/tally-server-${VERSION}.tar.gz /TallyClient/bin/Release/net6.0/linux-arm64/publish/
+
+git commit -m "Built new Version ${VERSION}"
+git push 
